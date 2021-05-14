161130805e64f957e0404b583eaae94b1466656b
komalsathe2112 <83824427+komalsathe2112@users.noreply.github.com>
2021-05-14 16:27:21 +0530

    Add files via upload

---
+++ Assignment2/Pattern1/P1_9.java
@@ -0,0 +1,14 @@
+class P1_9{
+	public static void main(String args[]){
+		for(char i='A';i<='E';i++){
+			for(char j=i;j<'E';j++){
+			System.out.print(" ");
+			}
+			for(char j='A';j<=i;j++){
+				System.out.print(" "+j);
+			}
+			System.out.println();
+		}
+
+	}
+}
